7308f9a5cbf47933cda7bef10479537c2dbe103c
coda <coda@local>
2025-04-08 09:49:04 +0800

    .

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,8 +1,11 @@
-import { AppShell, Burger, Button, Grid, Group, UnstyledButton, TextInput, Text, Container, Flex } from "@mantine/core";
+import { AppShell, Burger, Button, Grid, Group, UnstyledButton, TextInput, Text, Container, Flex, Stack } from "@mantine/core";
 import { Link } from "@inertiajs/react";
 import { IconSearch } from "@tabler/icons-react";
 import { useForm } from "@inertiajs/react";
+import { useDisclosure } from "@mantine/hooks";
 export default function Layout({ children }) {
+
+  const [opened, { toggle }] = useDisclosure(false);
 
   const { data, setData, post, errors, processing } = useForm({
     search: "",
@@ -19,35 +22,60 @@ export default function Layout({ children }) {
   return (
     <AppShell
       padding="xl"
-      >
+      navbar={{
+        width: 300, 
+        breakpoint: "sm",
+        collapsed: {
+          desktop: true,
+          mobile: !opened 
+        }
+      }}
+    >
+
       <AppShell.Header
         py="md"
         >
         <Grid>
-          <Grid.Col span={2}>
-            <Text fw={700} size="xl">FactsHub</Text>
+          <Grid.Col span={{base: 2, md: 2}} visibleFrom="md">
+            <Text fw={700} size="xl" visibleFrom="md">FactsHub</Text>
           </Grid.Col>
-          <Grid.Col span={8}>
+          <Grid.Col span={{base: 10, md: 8}}>
             <Group justify="center">
-              <form onSubmit={handleSubmit}>
+              <Burger
+                hiddenFrom="sm"
+                opened={opened}
+                onClick={toggle}
+                size="sm"
+                color="gray"
+                />                  
+              <form onSubmit={handleSubmit}>                
                 <Group gap="0.2rem">
-                  <TextInput w="40rem" placeholder="Search" value={data.search} onChange={(e) => setData("search", e.target.value)}/>
-                  <Button type="submit" variant="default">
+                  <TextInput placeholder="Search" value={data.search} onChange={(e) => setData("search", e.target.value)}/>
+                  <Button type="submit" variant="default" >
                     <IconSearch size={16} />
                   </Button>
                 </Group>
               </form>
-              <Button component={Link} href="/submit">Submit a Report</Button>
+              <Button mw="4rem" component={Link} href="/submit">Submit a Report</Button>
             </Group>
           </Grid.Col>
-          <Grid.Col span={2}>
-            <Flex justify="flex-end" gap="sm" align="center" h="100%">
+          <Grid.Col span={{base: 0, md: 2}} visibleFrom="md">
+            <Flex justify="flex-end" gap="sm" align="center" h="100%" visibleFrom="md">
               <UnstyledButton component={Link} href="/sign-in">Login</UnstyledButton>
               <UnstyledButton component={Link} href="/sign-up">Register</UnstyledButton>
             </Flex>
           </Grid.Col>
         </Grid>
       </AppShell.Header>
+
+      <AppShell.Navbar>
+        <Stack>
+          <UnstyledButton component={Link} href="/sign-in">Login</UnstyledButton>
+          <UnstyledButton component={Link} href="/sign-up">Register</UnstyledButton>
+        </Stack>
+      </AppShell.Navbar>
+
+
       <AppShell.Main pt="4rem">
         {children}
       </AppShell.Main>
